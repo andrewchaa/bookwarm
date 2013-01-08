b2ef5bf9fad724a972604028764ecea3d8f9f3ad
Andrew Chaa <andrew.chaa@huddle.com>
2013-01-08 18:10:21 +0000

    restoring the page

--- Bookwarm/Scripts/bookwarm.js
+++ Bookwarm/Scripts/bookwarm.js
@@ -1,12 +1,17 @@
 ﻿$(function () {
-    var pageNumberCheck = function() {
-        var pageNumber = $('#pageNumber').val();
-        console.log(pageNumber);
-        $.post('/api/page', {
-            userName: 'Andrew',
-            pageNumber: pageNumber,
-            title: document.title
-        });
-    };
-    window.setInterval(pageNumberCheck, 20000);
+    var pageNumber = $('#pageNumber').val(),
+        pageNumberCheck = function () {
+            console.log(pageNumber);
+            if (!!document.title) {
+                
+                $.post('/api/page', {
+                    userName: 'Andrew',
+                    pageNumber: pageNumber,
+                    title: document.title
+                });
+            }
+        };
+
+    window.setInterval(pageNumberCheck, 5000);
+
 });
