ac9cb12f115102ddb4bd024d2fa0b92c07940d96
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2013-01-08 22:00:36 +0000

    building a new page

--- Bookwarm/Controllers/HomeController.cs
+++ Bookwarm/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace Bookwarm.Controllers
             return View();
         }
 
+        public ActionResult Test()
+        {
+            return View();
+        }
+
     }
 }
--- Bookwarm/Scripts/bookwarm.js
+++ Bookwarm/Scripts/bookwarm.js
@@ -1,16 +1,15 @@
 ﻿$(function () {
-    var pageNumber = $('#pageNumber').val(),
-        pageNumberCheck = function () {
-            console.log(pageNumber);
-            if (!!document.title) {
-                
-                $.post('/api/page', {
-                    userName: 'Andrew',
-                    pageNumber: pageNumber,
-                    title: document.title
-                });
-            }
-        };
+    var pageNumberCheck = function () {
+        var pageNumber = $('#pageNumber').val();
+        console.log(pageNumber);
+        if (!!document.title) {
+            $.post('/api/page', {
+                userName: 'Andrew',
+                pageNumber: pageNumber,
+                title: document.title
+            });
+        }
+    };
 
     window.setInterval(pageNumberCheck, 5000);
 
