a35492bfbb345e2cc9a199e5ae348d6dc61bd3ca
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2013-01-17 20:32:51 +0000

    use knockout js

--- Bookwarm/Scripts/bookwarm.js
+++ Bookwarm/Scripts/bookwarm.js
@@ -1,9 +1,12 @@
 ﻿$(function () {
+    var isTitleChanged = false,
+        bookTitle;
     var pageNumberCheck = function () {
         var pageNumber = $('#pageNumber').val();
         console.log(pageNumber);
+
         if (!!document.title) {
-            $.post('/api/page', {
+            $.post('/api/', {
                 userName: 'Andrew',
                 pageNumber: pageNumber,
                 title: document.title
